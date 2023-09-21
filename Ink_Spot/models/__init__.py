f2e8d224b133f844fb16fb85346e0947d14f1ab9
Abrar Shaik <78590172+devShaik010@users.noreply.github.com>
2023-09-21 20:30:13 +0530

    Add files via upload

---
+++ Ink_Spot/models/__init__.py
@@ -0,0 +1,6 @@
+from .product import Product
+from .categories import Categories
+from .book import Book
+from .b_catogeries import B_categories
+from .customer import Customer
+from .order import Order
